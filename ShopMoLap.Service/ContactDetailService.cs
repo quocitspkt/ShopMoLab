dd272a8e4f1783287760cd18b897dcd99c11f7ab
ThinhKT <thinhtm99@gmail.com>
2020-06-23 21:27:12 +0700

    cập nhật mới tầng service

---
+++ ShopMoLap.Service/ContactDetailService.cs
@@ -0,0 +1,32 @@
+﻿using ShopMoLab.Data.Infrastructure;
+using ShopMoLab.Data.Repositories;
+using ShopMoLab.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public interface IContactDetailService
+    {
+        ContactDetail GetDefaultContact();
+    }
+    public class ContactDetailService : IContactDetailService
+    {
+        IContactDetailRepository _contactDetailRepository;
+        IUnitOfWork _unitOfWork;
+
+        public ContactDetailService(IContactDetailRepository contactDetailRepository, IUnitOfWork unitOfWork)
+        {
+            this._contactDetailRepository = contactDetailRepository;
+            this._unitOfWork = unitOfWork;
+        }
+
+        public ContactDetail GetDefaultContact()
+        {
+            return _contactDetailRepository.GetSingleByCondition(x => x.Status);
+        }
+    }
+}
