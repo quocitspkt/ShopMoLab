46ba570b8ec341b07a7e349323ec9559589f9163
= <=>
2020-06-24 09:03:50 +0700

    -To chuc project web vaf dung web api -Ap dung Dependency su dung Autofac (add Autofac,Autofac.2api,Autofac.mvc5) -AutoMaper Web.Models (add Automaper, Onwin)

---
+++ ShopMoLap.Web/Mappings/AutoMapperConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ShopMoLap.Model.Models;
+using ShopMoLap.Web.Models;
+
+
+namespace ShopMoLap.Web.Mappings
+{
+    public class AutoMapperConfiguration
+    {
+        public static void Configure()
+        {
+            Mapper.CreateMap<Post, PostViewModel>();
+            Mapper.CreateMap<PostCategory, PostCategoryViewModel>();
+            Mapper.CreateMap<Tag, TagViewModel>();
+
+            Mapper.CreateMap<ProductCategory, ProductCategoryViewModel>();
+            Mapper.CreateMap<Product, ProductViewModel>();
+            Mapper.CreateMap<ProductTag, ProductTagViewModel>();
+            Mapper.CreateMap<Footer, FooterViewModel>();
+            Mapper.CreateMap<Slide, SlideViewModel>();
+            Mapper.CreateMap<Page, PageViewModel>();
+            Mapper.CreateMap<ContactDetail, ContactDetailViewModel>();
+
+            Mapper.CreateMap<ApplicationGroup, ApplicationGroupViewModel>();
+            Mapper.CreateMap<ApplicationRole, ApplicationRoleViewModel>();
+            Mapper.CreateMap<ApplicationUser, ApplicationUserViewModel>();
+        }
+    }
+}
