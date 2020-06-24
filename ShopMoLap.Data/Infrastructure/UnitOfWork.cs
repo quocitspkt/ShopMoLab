a1ff6132ee766474d3620cd2d757209d68e28c02
quocitspkt <quocitspkt@gmail.com>
2020-06-25 06:54:31 +0700

    Xay dung domain entity, data access layer

---
+++ ShopMoLap.Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopMoLap.Data.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IDbFactory dbFactory;
+        private ShopMoLapDbContext dbContext;
+
+        public UnitOfWork(IDbFactory dbFactory)
+        {
+            this.dbFactory = dbFactory;
+        }
+
+        public ShopMoLapDbContext DbContext
+        {
+            get { return dbContext ?? (dbContext = dbFactory.Init()); }
+        }
+
+        public void Commit()
+        {
+            DbContext.SaveChanges();
+        }
+    }
+}
---
+++ ShopMoLap.Data/Repositories/ErrorRepository.cs
@@ -0,0 +1,21 @@
+﻿using ShopMoLap.Data.Infrastructure;
+using ShopMoLap.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopMoLap.Data.Repositories
+{
+    public interface IErrorRepository : IRepository<Error>
+    {
+    }
+
+    public class ErrorRepository : RepositoryBase<Error>, IErrorRepository
+    {
+        public ErrorRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
