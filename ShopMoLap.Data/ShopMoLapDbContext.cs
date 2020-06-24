e56f414fcc3ca45bc379462c88aaf40e2651ee3f
Pham Duc Huy <duchuypham200@gmail.com>
2020-06-24 15:07:37 +0700

    Tich hop Identity vao he thong

---
+++ ShopMoLap.Data/ShopMoLapDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using ShopMoLap.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopMoLap.Data
+{
+    class ShopMoLapDbContext: IdentityDbContext<ApplicationUser>
+    {
+        public ShopMoLapDbContext() : base("ShopMoLapConnection")
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+        }
+        public static ShopMoLapDbContext Create()
+        {
+            return new ShopMoLapDbContext();
+        }
+    }
+}
